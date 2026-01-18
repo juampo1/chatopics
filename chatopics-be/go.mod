3f5ec74029f9384b8739c422ca5f02b5e11ca93f
Juan Pablo Ortiz <juamportiz@gmail.com>
2026-01-18 17:14:51 -0300

    add websocket configuration

--- chatopics-be/go.mod
+++ chatopics-be/go.mod
@@ -1,3 +1,5 @@
 module github.com/juampo1/chatopics/chatopics-be
 
 go 1.25.1
+
+require github.com/gorilla/websocket v1.5.3 // indirect
--- chatopics-be/main.go
+++ chatopics-be/main.go
@@ -1,7 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true }, //For now accept any connection
+}
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println("Realtime Chat")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Server start")
+	})
+
+	http.HandleFunc("/ws", serveWs)
+}
+
+// readet will listen for new messages
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	reader(ws)
 }
